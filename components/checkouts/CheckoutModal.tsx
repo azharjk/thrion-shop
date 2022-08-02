fa1fc824d2dfb1256334b496234af1d986229e48
azharjk <azhr.project@gmail.com>
2022-08-02 16:09:59 +0700

    Add pay functionality

--- components/checkouts/CheckoutModal.tsx
+++ components/checkouts/CheckoutModal.tsx
@@ -1,8 +1,39 @@
 import Link from "next/link";
 import ReactModal from "react-modal";
 import { NextPage } from "next";
+import { gql } from "@apollo/client";
+import { useState } from "react";
 
+import client from "@/services/apollo-client";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import { Checkout } from "@/interfaces/checkout";
+
+interface CreateEWalletCharge {
+  createEWalletCharge: {
+    actions: {
+      desktop_web_checkout_url: string;
+    };
+    charge_amount: number;
+    id: string;
+    reference_id: string;
+    status: string;
+  };
+}
+
+// FIXME: Now just handle the e-wallet payment
+const CREATE_PAYMENT = gql`
+  mutation CreatePayment($data: CreateEWalletChargeInput!) {
+    createEWalletCharge(data: $data) {
+      id
+      reference_id
+      status
+      charge_amount
+      actions {
+        desktop_web_checkout_url
+      }
+    }
+  }
+`;
 
 interface CheckoutModalProps {
   isOpen: boolean;
@@ -19,6 +50,41 @@ const CheckoutModal: NextPage<CheckoutModalProps> = ({
   checkoutResponse,
 }) => {
   const { id, name, product } = checkoutResponse!;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlePayment = async () => {
+    let url: string | undefined;
+
+    try {
+      setIsLoading(true);
+
+      const response = await client.mutate<CreateEWalletCharge>({
+        mutation: CREATE_PAYMENT,
+        variables: {
+          data: {
+            orderID: id,
+            referenceID: `ref-${id}:${name}`,
+            currency: "IDR",
+            amount: product.price,
+            checkoutMethod: "ONE_TIME_PAYMENT",
+            channelCode: "ID_DANA",
+            channelProperties: {
+              successRedirectURL: "https://google.com",
+            },
+          },
+        },
+      });
+
+      url = response.data?.createEWalletCharge.actions.desktop_web_checkout_url;
+    } catch (err) {
+      console.log("ERROR: @handlePayment: ", err);
+    } finally {
+      setIsLoading(false);
+    }
+
+    location.href = url!;
+  };
 
   return (
     <ReactModal
@@ -56,6 +122,18 @@ const CheckoutModal: NextPage<CheckoutModalProps> = ({
             className="block w-full border mt-2 py-2 uppercase font-semibold rounded-sm"
           >
             Okay
+          </button>
+          <button
+            onClick={handlePayment}
+            className="relative block w-full border mt-2 py-2 uppercase font-semibold rounded-sm"
+          >
+            {isLoading ? (
+              <div className="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-50 z-10 flex justify-center items-center">
+                <LoadingSpinner />
+              </div>
+            ) : (
+              <span>Pay with DANA</span>
+            )}
           </button>
           <Link href="/">See other products</Link>
         </div>
